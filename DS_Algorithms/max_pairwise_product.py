5f0bb6a2234904d6ec95d51f786a8cfde2c49cc5
Kang-Li (Stephen) Cheng <kangli.cheng@gmail.com>
2017-05-15 16:40:40 -0400

    accepted solution

--- DS_Algorithms/max_pairwise_product.py
+++ DS_Algorithms/max_pairwise_product.py
@@ -2,12 +2,27 @@
 n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
+#a=[4, 3, 2, 5]
 
-result = 0
+if(len(a)<=1):
+    print (0)
+else:        
+    n=len(a)
+    a.sort()
+    print(max(a[-1]*a[-2], a[0]*a[1]))
+'''
+a=[4, 5, 6, 7]
+b=[1]
+c=[]
+d=[-4, -5, -6, -5]
+maxPairWise(a)
+maxPairWise(b)
+maxPairWise(c)
+maxPairWise(d)
+'''
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
+#maxElement=max(a)
+#a1=a.pop(a.index(max(a)))
+#maxElement2=max(a1)
 
-print(result)
+#print(maxElement*maxElement2)
